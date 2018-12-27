69f01d9782401a49d4e4917aa690f5ebe26873bc
Jesse Kelly <kellyjesse881@gmail.com>
2018-12-27 23:59:18 +0000

    Initilized

---
+++ boot.sh
@@ -0,0 +1,15 @@
+# general
+emacs --daemon &
+
+# graphical
+python -m pywal -R
+xset r rate 200 40 #Keypress rate
+setxkbmap -option caps:swapescape #ESC -> CAPS
+
+source "$HOME/.cache/wal/colors.sh" #Source Color Scheme. Dunst.
+
+dunst -lb "$color1" -nb "$color2" -hb "$color3" &
+compton --respect-prop-shadow &
+redshift -b 1.0:0.3 &
+urxvtd &
+unclutter -idle 2 -jitter 10 -visible &
---
+++ zsh/aliases.sh
@@ -0,0 +1,72 @@
+alias c="clear"
+alias t="touch"
+alias e="$EDITOR"
+alias ed="emacs --daemon"
+alias r="ranger"
+alias o="rifle" #Open
+alias s="sudo"
+alias h='history'
+alias hs='history | grep'
+alias k="pkill"
+alias q="exit"
+alias Q="echo \"Shutting Down..\"; sudo -k shutdown now"
+
+#paths
+alias home="cd $USER_HOME"
+alias conf="cd $CONFIG"
+alias docs="cd $DOCS"
+
+#frequent commands
+alias hs='history | grep '
+alias note="$EDITOR $DOCS/notes/$(date +'%F').org"
+
+#edit
+alias Ea="$EDITOR $CONFIG/zsh/aliases.sh" #Edit Aliases
+alias E3="$EDITOR ~/Dropbox/env/i3/config"
+alias Ez="$EDITOR $CONFIG/zsh/zshrc.sh" #Edit zsh config
+alias Es="$EDITOR /etc/ssh/sshd_config" #SSH Config
+alias Ei="$EDITOR $HOME/Dropbox/env/scripts/boot.sh"
+#Et: tmux
+#Ee: emacs. Eeb:bindings, Eep:packages, etc.
+#Er: ranger
+#ER: rifle
+
+#Ec: Edit clipboard in editor. 
+alias Ec="xclip -i -selection clipboard -o >> /tmp/clipEdit; $EDITOR /tmp/clipEdit; cat /tmp/clipEdit | xclip -selection clipboard"
+
+#start
+alias Sp="cd ~; polybar bar -r &" #Start polybar
+
+#create
+alias Cpass="openssl rand -base64 20"
+alias Clabel="figlet"
+
+#web stuff
+alias serve="python -m SimpleHTTPServer 8000" 
+alias ipg="curl ipinfo.io/ip"
+alias ipl="ifconfig | awk '$1 == \"inet\" {print $2}'"
+alias up="ping 8.8.8.8"
+
+# grc - better colors
+if [[ -x `which grc` ]]; then
+	  alias grc='grc --colour=auto'
+	  alias ping='grc ping -c 3'
+	  alias last='grc last'
+	  alias netstat='grc netstat'
+	  alias traceroute='grc traceroute'
+	  alias diff='grc diff'
+	  alias gcc='grc gcc'
+	  alias configure='grc configure'
+	  alias cvs='grc cvs'
+	  alias gcc='grc gcc'
+	  alias configure='grc ./configure'
+	  alias cat="grc cat"
+fi
+
+#programs
+alias email="neomutt"
+alias calander="calcurse"
+alias alarm=". ~/Dropbox/env/scripts/alarm.sh"
+
+
+
